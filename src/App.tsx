b0775784179f1b8e8ca9abb45bd4aa8d5f0de307
amnabbouti <amine.abbouti@hotmail.com>
2025-02-14 12:54:35 +0100

    replace Home component with CocktailGrid in routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
 import CocktailGrid from './components/CocktailGrid';
 import CocktailDetail from './components/CocktailDetail';
 import './App.css';
@@ -8,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<CocktailGrid />} />
         <Route path="/cocktails" element={<CocktailGrid />} />
         <Route path="/cocktails/:id/:slug" element={<CocktailDetail />} />
       </Routes>
